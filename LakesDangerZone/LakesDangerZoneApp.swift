eb2cec59c4217c4f3243317f90be36dcfb2224be
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-09-07 19:00:40 -0300

    Initial Commit

---
+++ LakesDangerZone/LakesDangerZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LakesDangerZoneApp.swift
+//  LakesDangerZone
+//
+//  Created by Gustavo Barros da Silveira on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LakesDangerZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
