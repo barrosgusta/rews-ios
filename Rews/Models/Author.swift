a0652b8a2972dc9448f5ac288daa7f801f7cc27d
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-09-28 23:10:05 -0300

    Create Author.swift

---
+++ Rews/Models/Author.swift
@@ -0,0 +1,13 @@
+//
+//  Author.swift
+//  Rews
+//
+//  Created by Gustavo Barros da Silveira on 12/09/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let email, name, createdAt: String
+}
